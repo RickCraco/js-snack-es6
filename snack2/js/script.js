b6e16da2fa8941b8bc786fe78d7c6ab9e1a6de8d
RickCraco <riccardocracolici43@gmail.com>
2023-10-24 14:58:26 +0200

    aggiunto array contenente tutti gli studenti

--- snack2/js/script.js
+++ snack2/js/script.js
@@ -6,3 +6,12 @@ class Studente{
     }
 }
 
+const classe = [
+new Studente(213, 'Marco della Rovere',78),
+new Studente(110, 'Paola Cortellessa', 96),
+new Studente(250 ,'Andrea Mantegna', 48),
+new Studente(145, 'Gaia Borromini', 74),
+new Studente(196, 'Luigi Grimaldello', 68),
+new Studente(102, 'Piero della Francesca', 50),
+new Studente(120, 'Francesca da Polenta', 84)
+]
